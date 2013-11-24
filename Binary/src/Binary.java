b7e269113ec02f9caee442f852def1920d714773
stuie382 <stu382@hotmail.co.uk>
2013-11-24 11:21:28 +0000

    fix and tidy

--- Binary/src/Binary.java
+++ Binary/src/Binary.java
@@ -12,7 +12,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Binary extends JPanel implements ActionListener {
+public class Binary extends JPanel {
 
    private static final long serialVersionUID = -2803257217038020759L;
    // Panel for buttons
@@ -20,10 +20,10 @@ public class Binary extends JPanel implements ActionListener {
    // Panel for drawing on
    JPanel                    canvas;
    // buttons for the GUI
-   JButton                   convert;
+   JButton                   decToBinary;
    JButton                   reset;
-   JButton                   bindec;
-   JTextField                decimalstring;
+   JButton                   binaryToDec;
+   JTextField                userInput;
    JLabel                    outputarea;
    ImageIcon                 num0;
    ImageIcon                 num1;
@@ -38,8 +38,7 @@ public class Binary extends JPanel implements ActionListener {
    ImageIcon                 blank;
 
    public Binary() {
-      super(true); // Call constructor of parent
-      // Standard layout (flow)
+      super(true);
       setupImages();
       setupGUI();
    }
@@ -49,30 +48,109 @@ public class Binary extends JPanel implements ActionListener {
       // Set up two panels, control board and canvas
       board = new JPanel(true);
       canvas = new JPanel(true);
-      board.setPreferredSize(new Dimension(150, 200)); // set size of the
-      // panels on the frame
+      board.setPreferredSize(new Dimension(150, 200));
       canvas.setPreferredSize(new Dimension(500, 250));
-      // set line colour of the borders
       board.setBorder(BorderFactory.createLineBorder(Color.black));
       canvas.setBorder(BorderFactory.createLineBorder(Color.blue));
       // Create buttons and attach listeners
-      convert = new JButton("Decimal Convert");
-      convert.addActionListener(this);
-      bindec = new JButton("Binary Convert");
-      bindec.addActionListener(this);
+      decToBinary = new JButton("Decimal Convert");
+      decToBinary.addActionListener(new ActionListener() {
+
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            decimalConvert();
+         }
+      });
+      binaryToDec = new JButton("Binary Convert");
+      binaryToDec.addActionListener(new ActionListener() {
+
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            binaryConvert();
+         }
+      });
       reset = new JButton("Reset");
-      reset.addActionListener(this);
-      // add text input box and printing label
-      decimalstring = new JTextField(8);
-      decimalstring.setBorder(BorderFactory.createLineBorder(Color.black));
+      reset.addActionListener(new ActionListener() {
+
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            resetGui();
+         }
+      });      // add text input box and printing label
+      userInput = new JTextField(8);
+      userInput.setBorder(BorderFactory.createLineBorder(Color.black));
       outputarea = new JLabel("Please enter either a positive integer or a binary string");
       add(canvas);
       add(board);
-      board.add(convert); // add button to control panel
-      board.add(bindec); // add button to control panel
-      board.add(decimalstring); // add label to control panel
+      board.add(decToBinary); // add button to control panel
+      board.add(binaryToDec); // add button to control panel
+      board.add(userInput); // add label to control panel
       board.add(reset); // add reset button
       canvas.add(outputarea); // add label to canvas
+   }
+
+   protected void binaryConvert() {
+      int k;
+      int answer = 0;
+      double length;
+      String binary;
+      String answer2;
+      binary = userInput.getText();
+      resetGui();
+      // adjust value to count from 0
+      length = binary.length() - 1;
+      // set variable to increment by power of 2 for each pass through the loop
+      k = (int) Math.pow(2, length);
+      ; // cast the double as an integer
+      for (int j = 0; j <= length; j++) {
+         // set through binary string
+         if (binary.charAt(j) == 49) {
+            answer += k; // add power of answer to the answer
+            k = k / 2;
+         } else {
+            k = k / 2;
+         }
+      }
+      answer2 = "" + answer; // create final answer
+      // loop to print images for the answer
+      for (int count = 0; count <= (answer2.length() - 1); count++) {
+         // convert character in string to match numbered .gif file
+         JLabel label = new JLabel(new ImageIcon(answer2.charAt(count) + ".gif"));
+         canvas.add(label);
+         canvas.validate();
+      }
+   }
+
+   protected void decimalConvert() {
+      try {
+         int binary, answer;
+         String answer1 = "";
+         String intbinary = userInput.getText();
+         binary = Integer.parseInt(intbinary);
+         resetGui();
+         if (binary >= 0) {
+            do {
+               answer = binary % 2;
+               answer1 = "" + answer + "" + answer1;
+               binary = binary / 2;
+            } while (binary != 0);
+            // drawing the binary code
+            for (int i = 0; i <= (answer1.length() - 1); i++) {
+               // answer1.charAt(i) is ASCII value for the integer 1
+               // rename images in src folder to numbers.
+               JLabel label = new JLabel(new ImageIcon(answer1.charAt(i) + ".gif"));
+               canvas.add(label);
+               canvas.validate();
+            }
+         } else {
+            outputarea.setText("Invalid entry. Please enter a positive decimal number.");
+            userInput.setText("");
+         }
+      } catch (Exception ex) {
+         outputarea.setText("Invalid entry. Please enter a positive decimal number.");
+         userInput.setText("");
+         System.out.println("EXCEPTION:: " + ex);
+      }
    }
 
    private void setupImages() {
@@ -89,82 +167,12 @@ public class Binary extends JPanel implements ActionListener {
       blank = new ImageIcon("blank.gif");
    }
 
-   @Override
-   public void actionPerformed(ActionEvent e) {
-      if (e.getSource() == convert) {
-         // code for decimal-binary conversion
-         try {
-            int binary, answer;
-            String answer1 = "";
-            String intbinary = decimalstring.getText();
-            binary = Integer.parseInt(intbinary);
-            if (binary >= 0) {
-               do {
-                  answer = binary % 2;
-                  answer1 = "" + answer + "" + answer1;
-                  binary = binary / 2;
-               } while (binary != 0);
-
-               outputarea.setText(answer1);
-               outputarea.setVisible(true);
-               // drawing the binary code
-               for (int i = 0; i <= (answer1.length() - 1); i++) {
-                  // answer1.charAt(i) is ASCII value for the integer 1
-                  // convert character in string to numbered .gif file
-                  System.out.println(answer1.charAt(i));
-                  ImageIcon image = new ImageIcon(answer1.charAt(i) + ".gif");
-                  JLabel label = new JLabel(image); // rename images in src folder to numbers.
-                  canvas.add(label);
-                  canvas.validate();
-               }
-            } else {
-               outputarea.setText("Invalid entry. Please enter a positive decimal number.");
-               decimalstring.setText("");
-            }
-         } catch (Exception e1) {
-            outputarea.setText("Invalid entry. Please enter a positive decimal number.");
-            decimalstring.setText("");
-         }
-      }
-      if (e.getSource() == reset) {
-         outputarea.setText(""); // set text area's to blank
-         decimalstring.setText("");
-         canvas.removeAll();
-         canvas.repaint();
-         repaint();
-      }
-      // code to convert a binary string to decimal number
-      if (e.getSource() == bindec) {
-         int j, k, answer = 0; // declare variables for binary to decimal conversion
-         double length, i;
-         String binary, answer2;
-         binary = decimalstring.getText();
-         length = binary.length() - 1; // adjust value to count from 0
-         // set variable to increment by power of 2 for each pass through the loop
-         i = Math.pow(2, length);
-         k = (int) i; // cast the double as an integer
-         for (j = 0; j <= length; j++) {
-            // set through binary string
-            if (binary.charAt(j) == 49) {
-               answer = answer + k; // add power of answer to the answer
-               k = k / 2;
-            } else {
-               k = k / 2;
-            }
-         }
-         answer2 = "" + answer; // create final answer
-         outputarea.setText(answer2);
-         outputarea.setVisible(true);
-         // loop to print images for the answer
-         for (int count = 0; count <= (answer2.length() - 1); count++) {
-            // convert character in string to match numbered .gif file
-            System.out.println(answer2.charAt(count));
-            ImageIcon image = new ImageIcon(answer2.charAt(count) + ".gif");
-            JLabel label = new JLabel(image); // display the number image in a label
-            canvas.add(label); // add the label to the canvas
-            canvas.validate(); // needed to allow the reset button to function
-         }
-      }
+   protected void resetGui() {
+      outputarea.setText("");
+      userInput.setText("");
+      canvas.removeAll();
+      canvas.repaint();
+      repaint();
    }
 
    public static void main(String[] args) {
